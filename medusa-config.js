a5e406a89b7c686b479ea717b5f9fd317a09bde2
Mark <mark.allison75@gmail.com>
2023-05-07 05:03:58 -0400

    hope3

--- medusa-config.js
+++ medusa-config.js
@@ -23,8 +23,7 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "const ADMIN_CORS = process.env.ADMIN_CORS || 
-  "/http:\\/\\/localhost:7000\\d+$/";
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
