548499d907fc97a335f48bfc7f89acb01311db5f
Mark <mark.allison75@gmail.com>
2023-05-07 03:20:45 -0400

    2

--- medusa-config.js
+++ medusa-config.js
@@ -23,14 +23,17 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+  process.env.ADMIN_CORS ||
+  "rediss://:37T5yQpfqRIACA9rYXh4g3wnjRwN8HHhdN1s5NmxfgjBs68BdqGk7SzWBF8NXYcE@6u16br.stackhero-network.com:6380";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
-const DATABASE_TYPE = process.env.DATABASE_TYPE || "sqlite";
+const DATABASE_TYPE = process.env.DATABASE_TYPE || "postgres";
 const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-store";
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_URL =
+  process.env.REDIS_URL ||
+  "rediss://:37T5yQpfqRIACA9rYXh4g3wnjRwN8HHhdN1s5NmxfgjBs68BdqGk7SzWBF8NXYcE@6u16br.stackhero-network.com:6380";
 
 const plugins = [
   `medusa-fulfillment-manual`,
