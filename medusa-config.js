0c52fb99ef0a66324c7f6c867938fdf554fc5dec
Mark <mark.allison75@gmail.com>
2023-05-07 03:08:59 -0400

    Deployment1

--- medusa-config.js
+++ medusa-config.js
@@ -28,9 +28,9 @@ const ADMIN_CORS =
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
-const DATABASE_TYPE = process.env.DATABASE_TYPE || "sqlite";
-const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-store";
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const DATABASE_TYPE = process.env.DATABASE_TYPE || "postgres";
+const DATABASE_URL = process.env.DATABASE_URL || DATABASE_URL;
+const REDIS_URL = process.env.REDIS_URL || REDIS_URL;
 
 const plugins = [
   `medusa-fulfillment-manual`,
